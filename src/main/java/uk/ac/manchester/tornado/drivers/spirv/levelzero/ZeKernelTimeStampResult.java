ca16b64ab48d6341912e2ba7983ebed37e070a11
Juan Fumero <juan.fumero@manchester.ac.uk>
2021-08-17 14:04:10 +0200

    KernelTimeStamp print format fixed

--- src/main/java/uk/ac/manchester/tornado/drivers/spirv/levelzero/ZeKernelTimeStampResult.java
+++ src/main/java/uk/ac/manchester/tornado/drivers/spirv/levelzero/ZeKernelTimeStampResult.java
@@ -75,10 +75,14 @@ public class ZeKernelTimeStampResult {
     }
 
     public void printTimers() {
-        System.out.println("Global Start: " + this.globalKernelStart + " (cycles)");
-        System.out.println("Global End: " + this.globalKernelEnd + " (cycles)");
-        System.out.println("Context Start: " + this.contextKernelStart + " (cycles)");
-        System.out.println("Context End: " + this.contextKernelEnd + " (cycles)");
-        System.out.println("Elapsed Time: " + this.kernelElapsedTime + " (ns)");
+        if (hasBeenResolved()) {
+            System.out.println("Global Start: " + this.globalKernelStart + " (cycles)");
+            System.out.println("Global End: " + this.globalKernelEnd + " (cycles)");
+            System.out.println("Context Start: " + this.contextKernelStart + " (cycles)");
+            System.out.println("Context End: " + this.contextKernelEnd + " (cycles)");
+            System.out.println("Elapsed Time: " + this.kernelElapsedTime + " (ns)");
+        } else {
+            System.err.println("Timer has not been resolved yet: ");
+        }
     }
 }
